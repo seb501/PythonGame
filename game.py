63d4c46d723e67a4ddc98a1132db4fe49b6ad7a8
Sebastian Titsch <titsch@th-brandenburg.de>
2024-06-15 20:08:01 +0200

    cammera + clouds

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
@@ -21,20 +22,35 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
         }
         
         self.player = PhysicsEntity(self, 'player' , (50,50), (8,15))
         self.tilemap = Tilemap(self, tile_size=16)
+        self.scroll = [0,0]
+        self.Clouds =  Clouds(self.assets['clouds'], count =  15)
+
+
     def run(self):
 
      
         while True:
             
-            self.display.fill((53,119,208))
-            self.tilemap.render(self.display)
+            self.display.blit(self.assets['background'], (0,0))
+            
+            #Camera
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 26
+            self.scroll[1] += (self.player.rect().centery - self.display.get_width() / 2 - self.scroll[1]) / 26
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.Clouds.update()
+            self.Clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset = render_scroll)
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
 
             
             for event in pygame.event.get():
@@ -47,6 +63,8 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = False
--- scripts/entities.py
+++ scripts/entities.py
@@ -8,9 +8,15 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0,0]
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
+
     def rect(self):
         return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    
+    
     def update(self, tilemap, movement=(0,0)):
+        self.collisions = {'up': False, 'down': False, 'right': False, 'left': False}
         frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
         
@@ -21,8 +27,10 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[0] > 0:
                     entity_rect.right =  rect.left
+                    self.collisions['right'] = True
                 if frame_movement[0] < 0:
                     entity_rect.left = rect.right
+                    self.collisions['left'] = True
                 self.pos[0] = entity_rect.x
 
 
@@ -32,10 +40,18 @@ class PhysicsEntity:
             if entity_rect.colliderect(rect):
                 if frame_movement[1] > 0:
                     entity_rect.bottom =  rect.top
+                    self.collisions['down'] = True
                 if frame_movement[1] < 0:
                     entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
                 self.pos[1] = entity_rect.y
+
         #gravity 
         self.velocity[1] = min(5, self.velocity[1] + 0.1)
-    def render(self, surf):
-        surf.blit(self.game.assets['player'], self.pos)
+        if self.collisions['down'] or self.collisions['up']:
+            self.velocity[1] = 0
+
+
+
+    def render(self, surf, offset=(0,0)):
+        surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
